50407e33a7cda00c40c385f9ec8b012838716d6c
vlachvojta <vojtik.vlach@seznam.cz>
2024-02-17 09:58:01 +0100

    Add basic server from folium tutorials.

---
+++ main.py
@@ -0,0 +1,91 @@
+""" main.py
+
+    Required packages:
+    - flask
+    - folium
+
+    Usage:
+    Start the flask server by running:
+        $ python main.py
+
+    And then head to http://127.0.0.1:5000/ in your browser to see the map displayed
+    Example taken from: https://python-visualization.github.io/folium/latest/advanced_guide/flask.html
+"""
+
+from flask import Flask, render_template_string
+
+import folium
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def fullscreen():
+    """Simple example of a fullscreen map."""
+    m = folium.Map(location=(50.0348411, 15.7775986))
+    return m.get_root().render()
+
+
+@app.route("/iframe")
+def iframe():
+    """Embed a map as an iframe on a page."""
+    m = folium.Map(location=(50.0348411, 15.7775986))
+
+    # set the iframe width and height
+    m.get_root().width = "800px"
+    m.get_root().height = "600px"
+    iframe = m.get_root()._repr_html_()
+
+    return render_template_string(
+        """
+            <!DOCTYPE html>
+            <html>
+                <head></head>
+                <body>
+                    <h1>Using an iframe</h1>
+                    {{ iframe|safe }}
+                </body>
+            </html>
+        """,
+        iframe=iframe,
+    )
+
+
+@app.route("/components")
+def components():
+    """Extract map components and put those on a page."""
+    m = folium.Map(
+        width=800,
+        height=600,
+        m = folium.Map(location=(50.0348411, 15.7775986))
+    )
+
+    m.get_root().render()
+    header = m.get_root().header.render()
+    body_html = m.get_root().html.render()
+    script = m.get_root().script.render()
+
+    return render_template_string(
+        """
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    {{ header|safe }}
+                </head>
+                <body>
+                    <h1>Using components</h1>
+                    {{ body_html|safe }}
+                    <script>
+                        {{ script|safe }}
+                    </script>
+                </body>
+            </html>
+        """,
+        header=header,
+        body_html=body_html,
+        script=script,
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
