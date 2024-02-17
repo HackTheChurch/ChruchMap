147be552a879a673523b6d67c90df0d0f50f7e89
vlachvojta <vojtik.vlach@seznam.cz>
2024-02-17 11:33:45 +0100

    Add more shit

--- database.py
+++ database.py
@@ -2,24 +2,47 @@ import pandas as pd
 
 class Database:
     def __init__(self, filename: str = 'churches.csv'):
-        self.db = pd.read_csv(filename)
+        churches_dicts = pd.read_csv(filename).to_dict(orient='records')
+        self.churches = []
+        for church in churches_dicts:
+            self.churches.append(Church.from_dict(church))
 
-    def get(self):
-        return self.db
+        # for church in self.churches:
+        #     print(church)
+
+        # print(self.db)
     
     def get_list(self):
-        return self.db.to_dict(orient='records')
+        return self.churches
 
 class Church:
-    def __init__(self, name: str, lat: float, lon: float):
+    def __init__(self, name: str, lat: float, lon: float, closest_mass: str):
         self.name = name
         self.lat = lat
         self.lon = lon
+        self.closest_mass = closest_mass
 
     def to_dict(self):
         return {
             'name': self.name,
             'lat': self.lat,
-            'lon': self.lon
+            'lon': self.lon,
+            'closest_mass': self.closest_mass
         }
 
+    def __str__(self):
+        return f'{self.name} - {self.lat} - {self.lon} - {self.closest_mass}'
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(data['name'], data['lat'], data['lon'], data['closest_mass'])
+
+def test_database():
+    db = Database()
+    # print(db.get_list())
+    churches = db.get_list()
+    for church in churches:
+        print(church)
+
+if __name__ == '__main__':
+    test_database()
--- main.py
+++ main.py
@@ -17,25 +17,25 @@ from flask import Flask, render_template_string, render_template
 import folium
 import pandas as pd
 
+from database import Database
+
 app = Flask(__name__)
-db = pd.read_csv('churches.csv')
+db = Database()
 
-# @app.route("/")
-# def fullscreen():
-#     """Simple example of a fullscreen map."""
-#     m = folium.Map(location=(50.0348411, 15.7775986))
-#     print(db)
-#     return m.get_root().render()
-
-
-#@app.route("/iframe")
 @app.route("/")
 def iframe():
     """Embed a map as an iframe on a page."""
     m = folium.Map(location=(50.0348411, 15.7775986))
 
+    for church in db.get_list():
+        folium.Marker(
+            [church.lat, church.lon], 
+            popup=f'{church.name}\n{church.closest_mass}',
+            # icon=folium.Icon(icon="church")
+        ).add_to(m)
+
     # set the iframe width and height
-    m.get_root().width = "800px"
+    # m.get_root().width = "800px"
     m.get_root().height = "600px"
     iframe = m.get_root()._repr_html_()
 
@@ -45,40 +45,40 @@ def iframe():
     )
 
 
-@app.route("/components")
-def components():
-    """Extract map components and put those on a page."""
-    m = folium.Map(
-        width=800,
-        height=600,
-        location=(50.0348411, 15.7775986)
-    )
+# @app.route("/components")
+# def components():
+#     """Extract map components and put those on a page."""
+#     m = folium.Map(
+#         width=800,
+#         height=600,
+#         location=(50.0348411, 15.7775986)
+#     )
 
-    m.get_root().render()
-    header = m.get_root().header.render()
-    body_html = m.get_root().html.render()
-    script = m.get_root().script.render()
+#     m.get_root().render()
+#     header = m.get_root().header.render()
+#     body_html = m.get_root().html.render()
+#     script = m.get_root().script.render()
 
-    return render_template_string(
-        """
-            <!DOCTYPE html>
-            <html>
-                <head>
-                    {{ header|safe }}
-                </head>
-                <body>
-                    <h1>Using components</h1>
-                    {{ body_html|safe }}
-                    <script>
-                        {{ script|safe }}
-                    </script>
-                </body>
-            </html>
-        """,
-        header=header,
-        body_html=body_html,
-        script=script,
-    )
+#     return render_template_string(
+#         """
+#             <!DOCTYPE html>
+#             <html>
+#                 <head>
+#                     {{ header|safe }}
+#                 </head>
+#                 <body>
+#                     <h1>Using components</h1>
+#                     {{ body_html|safe }}
+#                     <script>
+#                         {{ script|safe }}
+#                     </script>
+#                 </body>
+#             </html>
+#         """,
+#         header=header,
+#         body_html=body_html,
+#         script=script,
+#     )
 
 
 if __name__ == "__main__":
